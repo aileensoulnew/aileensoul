35a6cf4ea234258bdd1dfc99aba5cba2d4f8c62e
pratik-aileensoul <pratik.aileensoul@gmail.com>
2018-05-22 15:33:16 +0530

    Job
    remove deactivate

--- application/front/views/job_live/search_banner.php
+++ application/front/views/job_live/search_banner.php
@@ -21,11 +21,14 @@
     <div class="container">
         <div class="text-right pt20">
             <!--  -->
-            <?php if((!$isjobdeactivate || $isjobdeactivate == false) && $job_deactive == 0) { ?>
-                <a class="btn5" href="<?php echo $job_profile_link; ?>">Create Job Profile</a>
-            <?php } else{ ?>
-                <a class="btn5" href="<?php echo base_url('job/reactivateacc'); ?>">Reactivate Job Profile</a>
-            <?php } ?>
+            <?php
+            if($user_id != ""):
+                if((!$isjobdeactivate || $isjobdeactivate == false) && $job_deactive == 0) { ?>
+                    <a class="btn5" href="<?php echo $job_profile_link; ?>">Create Job Profile</a>
+                <?php } /*else{ ?>
+                    <a class="btn5" href="<?php echo base_url('job/reactivateacc'); ?>">Reactivate Job Profile</a>
+                <?php } */
+            endif; ?>
         </div>
         <div class="row">
             <div class="col-md-6" data-aos="fade-up" data-aos-duration="1000">
