a7f72f8525882120b72ae6ec680f37aa36450d18
harshad-aileensoul <harshad.aileensoul@gmail.com>
2018-06-02 19:34:34 +0530

    css bus pages

--- application/front/views/business_live/search.php
+++ application/front/views/business_live/search.php
@@ -175,7 +175,83 @@
             </div>
         </div>
 
+		<div>
+			<div class="mob-filter" data-target="#filter" data-toggle="modal">
+				<svg width="25.000000pt" height="25.000000pt" viewBox="0 0 300.000000 300.000000">
+					<g transform="translate(0.000000,300.000000) scale(0.100000,-0.100000)"
+					fill="#1b8ab9" stroke="none">
+					<path d="M489 2781 l-29 -29 0 -221 0 -221 -110 0 c-115 0 -161 -12 -174 -45
+					-3 -9 -6 -163 -6 -341 l0 -325 25 -24 c23 -24 30 -25 144 -25 l121 0 2 -646 3
+					-646 24 -19 c33 -27 92 -25 119 4 l22 23 0 642 0 642 124 0 c107 0 127 3 147
+					19 l24 19 3 331 3 332 -30 29 c-29 30 -30 30 -150 30 l-121 0 0 225 0 226 -25
+					24 c-34 35 -78 33 -116 -4z m271 -851 l0 -210 -210 0 -210 0 0 210 0 210 210
+					0 210 0 0 -210z"/>
+					<path d="M1445 2785 l-25 -24 0 -641 0 -640 -119 0 c-105 0 -121 -2 -145 -21
+					l-26 -20 0 -338 0 -338 23 -21 c21 -20 34 -22 145 -22 l122 0 0 -224 c0 -211
+					1 -225 21 -250 16 -21 29 -26 64 -26 35 0 48 5 64 26 20 25 21 39 21 250 l0
+					224 123 0 c181 0 167 -33 167 382 l0 337 -26 20 c-24 19 -40 21 -145 21 l-119
+					0 0 640 0 641 -25 24 c-33 34 -87 34 -120 0z m275 -1685 l0 -210 -215 0 -215
+					0 0 210 0 210 215 0 215 0 0 -210z"/>
+					<path d="M2405 2785 l-25 -24 0 -226 0 -225 -121 0 c-120 0 -121 0 -150 -30
+					l-30 -29 3 -332 3 -331 24 -19 c20 -16 40 -19 147 -19 l124 0 0 -643 0 -644
+					23 -21 c29 -28 86 -29 118 -3 l24 19 3 646 2 646 121 0 c114 0 121 1 144 25
+					l25 24 0 325 c0 178 -3 332 -6 341 -13 33 -59 45 -174 45 l-110 0 0 221 0 221
+					-29 29 c-38 37 -82 39 -116 4z m265 -855 l0 -210 -210 0 -210 0 0 210 0 210
+					210 0 210 0 0 -210z"/>
+					</g>
+					</svg>
+			</div>
+		</div>
+		<div id="filter" class="modal mob-modal fade" role="dialog">
+            <div class="modal-dialog modal-lm">
+                <div class="modal-content">
+                    <button type="button" class="modal-close" data-dismiss="modal">&times;</button>         
+                    <div class="mid-modal-body">
+						<div class="mid-modal-body">
+							<div class="left-search-box list-type-bullet">
+								<div class="">
+									<h3>Top Categories</h3>
+								</div>
+								<ul class="search-listing custom-scroll">
+									<li id="wow" ng-repeat="category in businessCategory">
+										<label class="control control--checkbox">
+											<span>{{category.industry_name | capitalize}}
+												<span class="pull-right">({{category.count}})</span>
+											</span>
+											<input class="categorycheckbox" type="checkbox" name="{{category.industry_name}}" value="{{category.industry_id}}" style="height: 12px;" [attr.checked]="(category.isselected) ? 'checked' : null" autocomplete="false">
+										   <div class="control__indicator"></div>
 
+										</label>
+									</li>
+								</ul>
+								<p class="text-left p10">
+									<a href="<?php echo business_category_list; ?>">View More Categories</a>
+								</p>
+							</div>
+							<div class="left-search-box list-type-bullet">
+								<div class="">
+									<h3>Top Locations</h3>
+								</div>                        
+								<ul class="search-listing" style="list-style: none;">
+									<li ng-repeat="location in businessLocation">
+										<label class="control control--checkbox">
+											<span>{{location.city_name | capitalize}}
+												<span class="pull-right">({{location.count}})</span>
+											</span>
+											<input class="locationcheckbox" type="checkbox" name="{{location.city_name}}" value="{{location.city_id}}" style="height: 12px;" [attr.checked]="(location.isselected) ? 'checked' : null" autocomplete="false">
+											<div class="control__indicator"></div>
+										</label>
+									</li>
+								</ul>
+								<p class="text-left p10">
+									<a href="<?php echo business_location_list; ?>">View More Locations</a>
+								</p>
+							</div>
+						</div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <script src="<?php echo base_url('assets/js/bootstrap.min.js?ver=' . time()) ?>"></script>
         <script src="<?php echo base_url('assets/js/aos.js?ver=' . time()) ?>"></script> 
         <script src="<?php echo base_url('assets/js/owl.carousel.min.js?ver=' . time()) ?>"></script>
@@ -198,5 +274,14 @@
         <script src="<?php echo base_url('assets/js/webpage/business-live/searchBusiness.js?ver=' . time()) ?>"></script>
         <script src="<?php echo base_url('assets/js/webpage/business-live/search.js?ver=' . time()) ?>"></script>
         <script src="<?php echo base_url('assets/js/webpage/business-profile/common.js?ver=' . time()) ?>"></script>
+		<script>
+			  window.onload = function(){
+				 var wow = document.getElementById("wow");
+				 wow.onclick = function(){
+					 alert("hi");
+				 }
+			  };
+				 
+		</script>
     </body>
 </html>
