8bf08b1179fc563ca81c359fcc9d4b8218cccaec
harshad-aileensoul <harshad.aileensoul@gmail.com>
2018-06-05 14:42:25 +0530

    footer

--- application/front/views/leftfooter.php
+++ application/front/views/leftfooter.php
@@ -1,34 +1,18 @@
-<div class="custom_footer_left fullwidth fw">
-    <div class="fl">
-        <ul class="cus-full1">
-            <li><a href="<?php echo base_url('about-us'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> About Us </a></li>
-
-            <li><a href="<?php echo base_url('contact-us'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Contact Us</a></li>
-            <li><a title="Sitemap" href="<?php echo base_url('sitemap'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Sitemap</a></li>
-            <?php
-
-                /*if (!$this->session->userdata('aileenuser')) {
-            ?>
-                <li><a title="Sitemap" href="<?php echo base_url('sitemap'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Sitemap</a></li>
-            <?php
-                }*/
-            ?>
-            <li><a href="<?php echo base_url('privacy-policy'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Privacy Policy</a></li>
-        </ul>
-        <ul class="cus-full2">
-            <li><a href="<?php echo base_url('terms-and-condition'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Terms &amp; Condition </a></li>
-            <li><a title="Disclaimer Policy" href="<?php echo base_url('disclaimer-policy'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Disclaimer Policy</a></li>
-            <li><a href="<?php echo base_url('blog'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Blog</a></li>
-        </ul>
-        <ul class="cus-full2">
-            <li><a href="<?php echo base_url('faq'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Faq </a></li>
-            <li><a title="Disclaimer Policy" href="<?php echo base_url('report-abuse'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Report </a></li>
-        </ul>
-        <ul class="cus-full2">
-            <li><a href="<?php echo base_url('feedback'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Send Us Feedback</a></li>
-            <li><a href="<?php echo base_url('advertise-with-us'); ?>" target="_blank"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Advertise With Us</a></li>
-
-            <li><a id="trigger" class="click-profiles" style="" href="javascript:void(0)"><span class="custom_footer_dot" role="presentation" aria-hidden="true"> · </span> Profiles <i class="fa fa-sort-asc" aria-hidden="true"></i></a>
+<div class="custom_footer_left fw">
+    <div class="">
+        <ul>
+            <li><a href="<?php echo base_url('about-us'); ?>" target="_blank">About</a></li>
+			<li><a href="<?php echo base_url('blog'); ?>" target="_blank">Blog</a></li>
+			<li><a href="<?php echo base_url('faq'); ?>" target="_blank">Faq</a></li>
+			<li><a href="<?php echo base_url('advertise-with-us'); ?>" target="_blank">Advertise With Us</a></li>
+			<li><a title="Sitemap" href="<?php echo base_url('sitemap'); ?>" target="_blank">Sitemap</a></li>
+			<li><a title="Report" href="<?php echo base_url('report-abuse'); ?>" target="_blank">Report </a></li>
+			<li><a href="<?php echo base_url('contact-us'); ?>" target="_blank">Contact</a></li>
+			<li><a href="<?php echo base_url('feedback'); ?>" target="_blank">Feedback</a></li>
+			<li><a href="<?php echo base_url('terms-and-condition'); ?>" target="_blank">Terms &amp; Condition </a></li>
+            <li><a href="<?php echo base_url('privacy-policy'); ?>" target="_blank">Privacy Policy</a></li>
+			<li><a title="Disclaimer Policy" href="<?php echo base_url('disclaimer-policy'); ?>" target="_blank">Disclaimer Policy</a></li>
+			<li><a id="trigger" class="click-profiles" style="" href="javascript:void(0)">Profiles <i class="fa fa-sort-asc" aria-hidden="true"></i></a>
                 <div class="click-nav">
                     <ul id="drop" class="left-ftr-profiles">
                         <li><a href="<?php echo base_url('how-to-use-job-profile-in-aileensoul'); ?>" target="_blank">  Job Profiles</a></li>
@@ -40,6 +24,7 @@
                 </div>
             </li>
         </ul>
+
     </div>
 </div>
 
