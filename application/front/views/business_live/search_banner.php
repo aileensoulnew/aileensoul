37094993ded54ca928bafbb0619fd665a011a431
pratik-aileensoul <pratik.aileensoul@gmail.com>
2018-06-05 22:13:20 +0530

    bug solve

--- application/front/views/business_live/search_banner.php
+++ application/front/views/business_live/search_banner.php
@@ -45,7 +45,7 @@
 	<div class="bottom-bnr-div">
 		<div class="container">
 			<p>Got a Business? Be Found by Your Audience
-				<a class="btn-1 pull-right" href="<?php echo $business_profile_link ?>" target="_self">Create Business Profile</a>
+				<a class="btn-1 pull-right" href="<?php echo base_url('business-profile/registration/business-information'); ?>" target="_self">Create Business Profile</a>
 			</p>
 		</div>
 	</div>
