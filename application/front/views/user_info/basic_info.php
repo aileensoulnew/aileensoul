ae41dd20ed57dacff1a880e925b070eba9041c14
harshad-aileensoul <harshad.aileensoul@gmail.com>
2018-06-06 19:12:40 +0530

    basic info

--- application/front/views/user_info/basic_info.php
+++ application/front/views/user_info/basic_info.php
@@ -5,7 +5,26 @@
             <form name="basicinfo" id="basicinfo" ng-submit="submitBasicInfoForm()" ng-validate="basicInfoValidate">
                 <div class="form-group">
                     <!--<p class="student-or-not">If you are a student then <a data-target="#Student-info" data-toggle="modal" href="javascript:;">Click Here.</a></p>-->
-                    <p class="student-or-not">If you are a student then <a href="educational-information">Click Here.</a></p>
+                    <p class="student-or-not">If Student then Make your Profile <a href="educational-information"> Here!</a> 
+						
+					</p> 
+					<p class="student-info-popup">
+						<a href="#">
+							<svg viewBox="0 0 65 65" xml:space="preserve" width="17px" height="17px">
+								<g>
+									<g>
+										<path d="M32.5,0C14.58,0,0,14.579,0,32.5S14.58,65,32.5,65S65,50.421,65,32.5S50.42,0,32.5,0z M32.5,61C16.785,61,4,48.215,4,32.5    S16.785,4,32.5,4S61,16.785,61,32.5S48.215,61,32.5,61z" fill="#5c5c5c"/>
+										<circle cx="33.018" cy="19.541" r="3.345" fill="#5c5c5c"/>
+										<path d="M32.137,28.342c-1.104,0-2,0.896-2,2v17c0,1.104,0.896,2,2,2s2-0.896,2-2v-17C34.137,29.237,33.241,28.342,32.137,28.342z    " fill="#5c5c5c"/>
+									</g>
+								</g>
+
+							</svg>
+						</a>
+						<span class="student-info-box">
+							If you are a student then fill “Educational Information” form to get relevant opportunities based on your interest, or else if you have graduated/working then fill the following form to get appropriate opportunities.
+						</span>
+					</p>
                 </div>
 
                 <div class="form-group">
@@ -48,4 +67,11 @@
     $("#jobTitle").focusout(function(){
         $('#jttooltip').hide();
     });
+	
+	
+</script>
+<script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
 </script>
