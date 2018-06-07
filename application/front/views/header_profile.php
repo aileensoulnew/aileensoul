bf3919e85903b3b56c1e0224ea047a0523df7e37
harshad-aileensoul <harshad.aileensoul@gmail.com>
2018-06-07 17:07:56 +0530

    mobile menu slide

--- application/front/views/header_profile.php
+++ application/front/views/header_profile.php
@@ -501,54 +501,27 @@ if($browser == "Firefox")
     <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right mob-side-menu" id="cbp-spmenu-s2">
 		<div class="all-profile-box content custom-scroll">
 			<ul class="all-pr-list">
-				<li>
-					<a href="<?php echo $job_right_profile_link; ?>">
-						<div class="all-pr-img">
-							<img src="https://www.aileensoul.com/assets/img/i1.png?ver=1517557803" alt="Job Profile">
-						</div>
-						<span>Job Profile</span>
-					</a>
-				</li>
-				<li>
-					<a href="<?php echo $recruiter_right_profile_link; ?>">
-						<div class="all-pr-img">
-							<img src="https://www.aileensoul.com/assets/img/i2.jpg?ver=1517557803" alt="Recruiter Profile">
-						</div>
-						<span>Recruiter Profile</span>
-					</a>
-				</li>
-				<li>
-					<a href="<?php echo $freelance_right_profile_link; ?>">
-						<div class="all-pr-img">
-							<img src="https://www.aileensoul.com/assets/img/i3.jpg?ver=1517557803" alt="Freelance Profile">
-						</div>
-						<span>Freelance Profile</span>
-					</a>
-				</li>
-				<li>
-					<a href="<?php echo $business_right_profile_link; ?>">
-						<div class="all-pr-img">
-							<img src="https://www.aileensoul.com/assets/img/i4.jpg?ver=1517557803" alt="Business Profile">
-						</div>
-						<span>Business Profile</span>
-					</a>
-				</li>
-				<li>
-					<a href="<?php echo $artist_right_profile_link; ?>">
-						<div class="all-pr-img">
-							<img src="https://www.aileensoul.com/assets/img/i5.jpg?ver=1517557803" alt="Artistic Profile">
-						</div>
-						<span>Artistic Profile</span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<div class="all-pr-img">
-							<img src="https://www.aileensoul.com/assets/img/i5.jpg?ver=1517557803" alt="Artistic Profile">
-						</div>
-						<span>more</span>
-					</a>
-				</li>
+				<li><a href="<?php echo $job_right_profile_link; ?>">Job Profile</a></li>
+				<li><a href="<?php echo $recruiter_right_profile_link; ?>">Recruiter Profile</a></li>
+				<li><a href="<?php echo $freelance_right_profile_link; ?>">Freelance Profile</a></li>
+				<li><a href="<?php echo $business_right_profile_link; ?>">Business Profile</a></li>
+				<li><a href="<?php echo $artist_right_profile_link; ?>">Artistic Profile</a></li>
+				<!--li>
+					<ul class="mob-footer-side"-->
+						
+                        <li><a title="About Us" href="<?php echo base_url('about-us'); ?>"  target="_blank">About</a></li>
+						<li><a title="Blog" href="<?php echo base_url('blog'); ?>" target="_blank">Blog</a></li>
+						<li><a title="Faq" tabindex="0" href="<?php echo base_url('faq'); ?>" target="_blank">FAQ</a></li>
+						<li><a title="Advertise With Us" href="<?php echo base_url('advertise-with-us'); ?>" target="_blank">Advertise With Us</a></li>
+						<!-- <li><a title="Sitemap" tabindex="0" href="<?php //echo base_url('sitemap'); ?>" target="_blank">Sitemap</a></li> -->
+						<li><a title="Report" tabindex="0" href="<?php echo base_url('report-abuse'); ?>" target="_blank">Report</a></li>
+						<li><a title="Contact Us" href="<?php echo base_url('contact-us'); ?>"  target="_blank">Contact</a></li>
+						<li><a title="Send Us Feedback" href="<?php echo base_url('feedback'); ?>" target="_blank">Feedback</a></li>
+						<li><a href="<?php echo base_url('terms-and-condition'); ?>" title="Terms and Condition" target="_blank">Terms and Condition</a></li>
+                        <li><a href="<?php echo base_url('privacy-policy'); ?>" title="Privacy policy" target="_blank">Privacy Policy</a></li>
+                        <li><a title="Disclaimer Policy" href="<?php echo base_url('disclaimer-policy'); ?>"  target="_blank">Disclaimer Policy</a></li>
+					<!--/ul>
+				</li-->
 			</ul>
 		</div>
 	</nav>
