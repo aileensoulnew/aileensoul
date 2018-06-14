11d1d01f70ecdac6d074c897391eb6cdd5229ccc
pratik-aileensoul <pratik.aileensoul@gmail.com>
2018-06-14 19:08:31 +0530

    Freelance Hire Bugs

--- application/front/views/freelancer_live/freelancer_hire/freelancer_hire_header2_new.php
+++ application/front/views/freelancer_live/freelancer_hire/freelancer_hire_header2_new.php
@@ -2,14 +2,14 @@
 $userid = $this->session->userdata('aileenuser');
 ?>
 <div id="fh_mob_search" class="modal fade mob-search-popup" role="dialog">
-	<form method="get">
+	<form action="<?php echo base_url('freelance-employer/search'); ?>" method="get" onSubmit="return checkformMob();">
 		<div class="new-search-input">
-			<input type="search" id="tags" class="tags" name="skills" value="" placeholder="Title, Keywords, or Skills" />
+			<input type="search" id="m_tags" class="tags" name="skills" value="" placeholder="Title, Keywords, or Skills" />
 			<input type="search" id="searchplace1" class="searchplace" name="searchplace" value="" placeholder="City, State or Country" />
 		</div>
 		<div class="new-search-btn">
 			<button type="button" class="close-new btn" data-dismiss="modal">Cancel</button>
-			<button type="submit" id="search_btn" class="btn btn-primary" onclick="return check();">Search</button>
+			<button type="submit" id="search_btn" class="btn btn-primary">Search</button>
 		</div>
 	</form>
 </div>
@@ -185,16 +185,16 @@ $userid = $this->session->userdata('aileenuser');
                 </div>
                 <div class="col-sm-6 col-md-5 col-xs-6 hidden-mob">
                     <div class="job-search-box1 clearfix">
-                        <form action="<?php echo base_url('freelance-employer/search'); ?>" method="get">
+                        <form action="<?php echo base_url('freelance-employer/search'); ?>" method="get" onSubmit="return checkform()">
                             <fieldset class="sec_h2">
-                                <input id="tags" class="tags ui-autocomplete-input skill_keyword" name="skills" placeholder="Title, Keywords, or Skills" autocomplete="off" type="text">
+                                <input id="d_tags" class="tags ui-autocomplete-input skill_keyword" name="skills" placeholder="Title, Keywords, or Skills" autocomplete="off" type="text">
                             </fieldset>
                             <fieldset class="sec_h2">
                                 <input id="searchplace" class="searchplace ui-autocomplete-input skill_keyword" name="searchplace" placeholder="City, State or Country" autocomplete="off" type="text">
                             </fieldset>
                             <fieldset class="new-search-btn">
                                 <label for="search_btn" id="search_f"><i class="fa fa-search" aria-hidden="true"></i></label>
-                                <input id="search_btn" style="display: none;" name="search_submit" value="Search" onclick="return checkvalue()" type="submit">
+                                <input id="search_btn" style="display: none;" name="search_submit" value="Search" type="submit">
                             </fieldset>
                         </form>    
                     </div>
@@ -477,12 +477,12 @@ $userid = $this->session->userdata('aileenuser');
 <!-- script for update all read msg notification start-->
 <script type="text/javascript">
     
-     $(document).ready(function () {
-      
-   var segment = '<?php echo "" . $this->uri->segment(1) . "" ?>';
-   if(segment != "chat"){ chatmsg(); };
-           });  // khyati chnages  start
- function chatmsg()
+	$(document).ready(function () {
+		var segment = '<?php echo "" . $this->uri->segment(1) . "" ?>';
+		if(segment != "chat"){ chatmsg(); };
+	});  // khyati chnages  start
+
+ 	function chatmsg()
     {             
              // khyati chnages  start
        
@@ -535,5 +535,22 @@ $userid = $this->session->userdata('aileenuser');
             }
         });
 
+    }
+
+    function checkform()
+    {    	
+    	if($("#d_tags").val() == "" && $("#searchplace").val() == "")
+    	{    		
+    		return false;
+    	}
+    	return true;
+    }
+    function checkformMob()
+    {    	
+    	if($("#m_tags").val() == "" && $("#searchplace1").val() == "")
+    	{    		
+    		return false;
+    	}
+    	return true;
     }
 </script>
