2ba29448b04cad80a2e588eef771258c1ec271e8
pratik-aileensoul <pratik.aileensoul@gmail.com>
2018-07-11 12:50:35 +0530

    Broken Image

--- assets/js/webpage/job/recommended_jobs.js
+++ assets/js/webpage/job/recommended_jobs.js
@@ -1,3 +1,14 @@
+app.directive('onErrorSrc', function() {
+    return {
+        link: function(scope, element, attrs) {
+          element.bind('error', function() {
+            if (attrs.src != attrs.onErrorSrc) {
+              attrs.$set('src', attrs.onErrorSrc);
+            }
+          });
+        }
+    }
+});
 app.filter('slugify', function () {
     return function (input) {
         if (!input)
