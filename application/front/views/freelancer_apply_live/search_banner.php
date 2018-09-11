7ed8e7b40a94cfb2b3428c2bf432e54aad4f5021
pratik-aileensoul <pratik.aileensoul@gmail.com>
2018-09-11 15:18:12 +0530

    change link

--- application/front/views/freelancer_apply_live/search_banner.php
+++ application/front/views/freelancer_apply_live/search_banner.php
@@ -61,7 +61,7 @@
 									
 								</li>
 								<li><a href="<?php echo base_url('login'); ?>">Login</a></li>
-								<li><a href="<?php echo base_url('business-profile/create-account'); ?>"><span class="hidden-479">Create Freelancer Profile</span><span class="visible-479">Sign Up</span></a></li>
+								<li><a href="<?php echo base_url('freelancer/create-account'); ?>"><span class="hidden-479">Create Freelancer Profile</span><span class="visible-479">Sign Up</span></a></li>
 							</ul>
 						</div>
 					</div>
