cb0147cd9f549f69edf982786fbc6fa9e2e6366f
pratik-aileensoul <pratik.aileensoul@gmail.com>
2018-10-16 15:32:55 +0530

    Article Changes

--- application/front/views/article/new_article.php
+++ application/front/views/article/new_article.php
@@ -206,6 +206,7 @@ $article_featured_upload_path = $this->config->item('article_featured_upload_pat
                         <div class="article-popup">
 							<?php $getFieldList = $this->data_model->getFieldList();?>
 							<fieldset class="fw">
+								<b>Select Category for your blog</b>
 								<span class="span-select">
 								<select name="article_main_category" id="article_main_category" onchange="other_field_fnc(this)">
 									<?php foreach ($getFieldList as $key => $value) { ?>
