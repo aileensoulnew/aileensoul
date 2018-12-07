0e8c7a34fbb9fbc5a26ef5da41eb03558d8afcd2
pratik-suthar <38644515+pratik-suthar@users.noreply.github.com>
2018-12-07 20:01:15 +0530

    test email

--- application/front/controllers/Test.php
+++ application/front/controllers/Test.php
@@ -13,6 +13,7 @@ class Test extends MY_Controller {
         $this->lang->load('message', 'english');
         $this->load->library('S3');
         $this->load->model('common');
+        $this->load->model('email_model');
 
 //        if (!$this->session->userdata('user_id')) {
 //            redirect('login', 'refresh');
@@ -25,6 +26,25 @@ class Test extends MY_Controller {
     public function index() {
         $user_id = $this->session->userdata('user_id');
         $this->load->view('test/amazon_add', $this->data);
+    }
+
+    public function testmail()
+    {
+        set_time_limit(0);
+        ini_set("memory_limit","512M");
+        
+        $to_email_id = array("mansiparmar911@gmail.com","mansiparmar911@yahoo.com","poorti.aileensoul@gmail.com","yatin.aileensoul@gmail.com","harshad.aileensoul@gmail.com","dhaval.aileensoul@gmail.com","pratik.aileensoul@gmail.com","adititrivedi231@gmail.com","bhattbhakti15@gmail.com","dshah1341@gmail.com","mansiparmar911@gmail.com","mansiparmar911@yahoo.com","poorti.aileensoul@gmail.com","yatin.aileensoul@gmail.com","harshad.aileensoul@gmail.com","dhaval.aileensoul@gmail.com","pratik.aileensoul@gmail.com","adititrivedi231@gmail.com","bhattbhakti15@gmail.com","dshah1341@gmail.com","dhavalshah@aileensoul.com");// $user_data['email'];
+        // echo $to_email_id = "mansiparmar911@gmail.com";
+
+        $email_html = $this->load->view('email_template/freelancer_test',$this->userdata,TRUE);
+
+        $subject = "Find Freelancing Work Opportunities on Aileensoul";
+        foreach ($to_email_id as $key => $value) {            
+            $send_email = $this->email_model->send_email_template($subject, $email_html, $to_email = $value,$unsubscribe);
+            var_dump($send_email);
+            echo "<br>".$value;
+        }
+        
     }
 
   
---
+++ application/front/views/email_template/freelancer_test.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mail</title>
+<style>
+	p{margin:0;}
+	h3{margin:0;}
+	.btn{
+		background: -moz-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* ff3.6+ */
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #3bb0ac), color-stop(56%, #1b8ab9), color-stop(100%, #1b8ab9)); /* safari4+,chrome */
+		background: -webkit-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* safari5.1+,chrome10+ */
+		background: -o-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* opera 11.10+ */
+		background: -ms-linear-gradient(96deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* ie10+ */
+		background: linear-gradient(354deg, #1b8ab9 0%, #1b8ab9 44%, #3bb0ac 100%); /* w3c */
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3bb0ac', endColorstr='#1b8ab9',GradientType=0 ); /* ie6-9 */
+		font-size:16px;
+		color:#fff;
+		padding:10px 25px;
+		text-decoration:none;
+	}
+	.btn:hover{/*opacity:0.8;*/}
+</style>
+</head>
+<body>
+	<div style="max-width:600px; margin:0 auto; background:#f4f4f4; padding:30px;">
+		<table width="100%" style="background:#fff" cellpadding="0" cellspacing="0">
+			<tr>
+				<td style="">
+					<table width="100%" cellpadding="0" cellspacing="0">
+						<tr>
+							<td style="text-align:center">
+								<h2>
+									<a style="color:#1b8ab9; text-decoration:none; font-size:23px;" href="#">Aileensoul</a>
+								</h2>
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td style="border-bottom:1px solid #ddd; padding:0 53px;">
+					<table width="100%" cellpadding="0" cellspacing="0">
+						<tr>
+							<td style="padding-top:15px;">
+								Hello,
+							</td>
+						</tr>
+						<tr>
+							<td style="padding:10px 0 0px; font-size:15px;">
+								<p style="margin:0;">Greetings from Aileensoul!</p>
+								
+							</td>
+						</tr>
+						<tr>
+							<td style="padding:10px 0 0px; font-size:15px;">
+								<p style="margin:0;">We saw that you are looking for a freelance work.</p>
+								
+							</td>
+						</tr>
+						<tr>
+							<td style="padding:10px 0 0px; font-size:15px;">
+								<p style="margin:0;">We understand that finding freelance projects seems difficult and time consuming.</p>
+								
+							</td>
+						</tr>
+						
+						<tr>
+							<td style="padding:10px 0 0px; font-size:15px;">
+								<p style="margin:0;">We are happy to inform you that we have built a new emerging platform for all freelancers like you to get projects and connect with freelance-employer easily without any hassle.</p>
+								
+							</td>
+						</tr>
+						<tr>
+							<td style="padding:10px 0 0px; font-size:15px;">
+								<p style="margin:0;">Register <a href="https://www.aileensoul.com/freelancer/create-account?utm_source=email&utm_medium=email&utm_campaign=freelancer&utm_term=signup" target="_blank"> here</a> to save and apply on jobs.</p>
+								
+							</td>
+						</tr>
+						<tr>
+							<td style="padding:10px 0 0px; font-size:15px;">
+								<p style="margin:0;">There are no hidden charges. It's FREE platform for everyone
+</p>
+								
+							</td>
+						</tr>
+						
+						<tr>
+							<td style="padding:10px 0 0px; font-size:15px;">
+								<p style="margin:0;"><b>To know more about our Freelancer profile, click<a href="https://www.aileensoul.com/freelance-jobs?utm_source=email&utm_medium=email&utm_campaign=freelancerlandingpage" target="_blank" > here.</a></b></p>
+								
+							</td>
+						</tr>
+						
+						
+						<tr>
+							<td style="padding-top:40px; padding-bottom:40px;">
+								<p style="padding-bottom:15px;">Thanks,</p>
+								<p><b>Aileensoul Team</b></p>
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+		<table width="100%" cellpadding="0" cellspacing="0">
+			<tr>
+				<td style="text-align:center;padding:30px 0 10px;font-size:12px">
+                         Aileensoul Technologies Private Limited<br>
+                        Titanium City Centre, 100 Feet Road, Satellite, Ahmedabad, India.
+                </td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
