b4281e597c9bdcd530a1eec049ca3a8b6908b1d1
pratik-suthar <38644515+pratik-suthar@users.noreply.github.com>
2018-12-24 17:03:30 +0530

    business changes

--- application/front/views/business_profile_live/business_profile_post.php
+++ application/front/views/business_profile_live/business_profile_post.php
@@ -135,7 +135,7 @@ $mobile_agent = $this->agent->mobile;
                                 </div>
                             </div>
 							
-                            <div class="custom-user-list" style="display: none;">
+                            <div class="custom-user-list-cus" style="display: none;">
                                 <?php
                                 if ($follow_user_suggest_count > 0) {
                                     ?>
