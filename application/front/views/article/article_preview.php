f2624ecdb06254b6c9d4ab256fc0ac97b5a7669e
pratik-suthar <38644515+pratik-suthar@users.noreply.github.com>
2018-12-29 15:45:35 +0530

    add canonical tag

--- application/front/views/article/article_preview.php
+++ application/front/views/article/article_preview.php
@@ -23,7 +23,7 @@ else
         <!-- <meta name="robots" content="noindex, nofollow"> -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php //echo $head; ?>
+        <link rel="canonical" href="<?php echo current_url(); ?>" />
         <link rel="icon" href="<?php echo base_url('assets/images/favicon.png?ver=' . time()); ?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/css/common-style.css?ver=' . time()) ?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/n-css/animate.css?ver=' . time()) ?>">
--- application/front/views/user_post/opprtunity_detail.php
+++ application/front/views/user_post/opprtunity_detail.php
@@ -5,7 +5,9 @@
         <meta name="description" content="<?php echo ($metadesc != '' ? $metadesc : ''); ?>" />
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <link rel="canonical" href="<?php echo current_url(); ?>" />
+
         <link rel="icon" href="<?php echo base_url('assets/images/favicon.png?ver=' . time()); ?>"> 
         <link rel="stylesheet" href="<?php echo base_url('assets/css/common-style.css') ?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/n-css/animate.css') ?>">
