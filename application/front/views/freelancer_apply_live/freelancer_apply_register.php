837edb02fceaf1d54114da266147a06dc72cc19e
pratik-suthar <38644515+pratik-suthar@users.noreply.github.com>
2018-12-29 14:58:03 +0530

    add canonical tag

--- application/front/views/freelancer_apply_live/freelancer_apply_register.php
+++ application/front/views/freelancer_apply_live/freelancer_apply_register.php
@@ -8,6 +8,7 @@
         <!-- <meta name="robots" content="noindex, nofollow"> -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="canonical" href="<?php echo current_url(); ?>" />
         <?php //echo $head; ?>
         <link rel="icon" href="<?php echo base_url('assets/images/favicon.png?ver=' . time()); ?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/css/common-style.css?ver=' . time()) ?>">
