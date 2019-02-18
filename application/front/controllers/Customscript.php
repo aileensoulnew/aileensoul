b48a5bc6e3cb84db07192a0353bd68ad4bb91cb0
pratik-suthar <38644515+pratik-suthar@users.noreply.github.com>
2019-02-18 16:01:27 +0530

    changes

--- application/front/controllers/Customscript.php
+++ application/front/controllers/Customscript.php
@@ -7,6 +7,7 @@ class Customscript extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model('common');
+        $this->load->library('S3');
     }
 
     public function index() {
@@ -88,5 +89,90 @@ class Customscript extends CI_Controller {
             $updatdata = $this->common->update_data($data, 'cities', 'city_id', $value->city_id);
         }
         echo "Done";
+    }
+
+    public function upload_move_to_amazon()
+    {
+        set_time_limit(0);
+        ini_set("memory_limit","512M");
+
+        $main_dir = scandir('uploads');
+        // print_r($main_dir);
+        $s3 = new S3(awsAccessKey, awsSecretKey);
+        echo "<pre>";
+        foreach ($main_dir as $k=>$_main_dir) {
+            /*if(is_dir($_main_dir))
+            {
+                echo $_main_dir."\n";   
+            }*/
+            // print_r($_main_dir);
+            $_main_dir_arr = explode(".", $_main_dir);
+            echo $_main_dir."\n";
+            // print_r($_main_dir_arr);
+            if(count($_main_dir_arr) < 2 && ($_main_dir == "artistic_profile" || $_main_dir == "artistic_bg" || $_main_dir == "artistic_post" || $_main_dir == "business_bg" || $_main_dir == "business_profile" || $_main_dir == "business_post" || $_main_dir == "job_profile" || $_main_dir == "job_bg" || $_main_dir == "recruiter_profile" || $_main_dir == "recruiter_bg" || $_main_dir == "freelancer_hire_profile" || $_main_dir == "freelancer_hire_bg" || $_main_dir == "freelancer_post_profile" || $_main_dir == "freelancer_post_bg" || $_main_dir == "user_post" || $_main_dir == "user_profile" || $_main_dir == "user_bg"))
+            {
+                // echo $_main_dir."-------------";
+                // $inner_dir = scandir('uploads/'.$_main_dir);
+
+                $inner_dir = array_diff(scandir('uploads/'.$_main_dir), array('..', '.'));
+                print_r($inner_dir);
+                foreach ($inner_dir as $_inner_dir) {
+                    /*$info = $s3->getObjectInfo(bucket, 'uploads/'.$_main_dir.'/'.$_inner_dir);
+                    echo 'uploads/'.$_main_dir.'/'.$_inner_dir;
+                    if($info)
+                    {
+                        echo "->Yes";
+                    }
+                    else
+                    {
+                        $s3->putObjectFile('uploads/'.$_main_dir.'/'.$_inner_dir, bucket, 'uploads/'.$_main_dir.'/'.$_inner_dir, S3::ACL_PUBLIC_READ);
+                        echo "->No";
+                    }
+                    echo "\n";*/
+                    // echo 'uploads/'.$_main_dir.'/'.$_inner_dir."-------------";
+
+                    $_inner_dir_arr = explode(".", $_inner_dir);
+                    if(count($_inner_dir_arr) < 2 && $_inner_dir != "main03062018" && $_inner_dir != "blog - Copy")
+                    {
+                        $inner_inner_dir = array_diff(scandir('uploads/'.$_main_dir.'/'.$_inner_dir), array('..', '.'));
+                        foreach ($inner_inner_dir as $_inner_inner_dir) {
+                            $info = $s3->getObjectInfo(bucket, 'uploads/'.$_main_dir.'/'.$_inner_dir.'/'.$_inner_inner_dir);
+                            echo 'uploads/'.$_main_dir.'/'.$_inner_dir.'/'.$_inner_inner_dir;
+                            if($info)
+                            {
+                                echo "->YES";
+                            }
+                            else
+                            {
+                                echo "->NO";
+                                // $s3->putObjectFile('uploads/'.$_main_dir.'/'.$_inner_dir.'/'.$_inner_inner_dir, bucket, 'uploads/'.$_main_dir.'/'.$_inner_dir.'/'.$_inner_inner_dir, S3::ACL_PUBLIC_READ);
+                            }
+                            echo "\n";
+                        }
+                    }
+                    else
+                    {
+                        $info = $s3->getObjectInfo(bucket, 'uploads/'.$_main_dir.'/'.$_inner_dir);
+                        echo 'uploads/'.$_main_dir.'/'.$_inner_dir;
+                        if($info)
+                        {
+                            echo "->YES";
+                        }
+                        else
+                        {
+                            // $s3->putObjectFile('uploads/'.$_main_dir.'/'.$_inner_dir, bucket, 'uploads/'.$_main_dir.'/'.$_inner_dir, S3::ACL_PUBLIC_READ);
+                            echo "->NO";
+                        }
+                    }
+                }
+                // echo "\n";
+                // print_r($inner_dir);
+            }
+
+            /*if($k == 10){
+                echo "string";
+                exit();
+            }*/
+        }
     }
 }
--- application/front/controllers/Userprofile.php
+++ application/front/controllers/Userprofile.php
@@ -47,7 +47,7 @@ class Userprofile extends MY_Controller {
         $this->data['is_userStudentInfo'] = $this->user_model->is_userStudentInfo($userid);
         $this->data['is_userSlugBasicInfo'] = $this->user_model->getUserProfessionDataBySlug($userslug, $data = "jt.name as Designation,it.industry_name as Industry,c.city_name as City");
         $this->data['is_userSlugStudentInfo'] = $this->user_model->getUserStudentDataBySlug($userslug, $data = "d.degree_name as Degree,u.university_name as University,c.city_name as City");
-        $this->data['is_userPostCount'] = $this->user_post_model->userPostCountBySlug($userslug);
+        // $this->data['is_userPostCount'] = $this->user_post_model->userPostCountBySlug($userslug);
         $fullname = $userdata['first_name']." ".$userdata['last_name'];
         if(isset($this->data['is_userSlugBasicInfo']) && !empty($this->data['is_userSlugBasicInfo']))
         {
@@ -92,7 +92,6 @@ class Userprofile extends MY_Controller {
         $this->data['header'] = $this->load->view('userprofile/header', $this->data, TRUE);
         $this->data['login_footer'] = $this->load->view('login_footer', $this->data, TRUE);
         // $this->data['footer'] = $this->load->view('userprofile/footer', $this->data, TRUE);
-        
 
         if (count($userdata) > 0) {
             $this->load->view('userprofile/index', $this->data);
