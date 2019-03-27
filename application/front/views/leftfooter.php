16d11f88f73d9916606d8a6e33813bcf191b7f31
harshad-aileensoul <harshad.aileensoul@gmail.com>
2019-03-27 18:53:01 +0530

    left footer css changes

--- application/front/views/leftfooter.php
+++ application/front/views/leftfooter.php
@@ -8,10 +8,11 @@
             <!-- <li><a href="<?php //echo base_url('faq'); ?>" target="_self">FAQ</a></li> -->
             <li><a title="Sitemap" href="<?php echo base_url('sitemap'); ?>" target="_self">Sitemap</a></li>
             <li><a href="<?php echo base_url('advertise-with-us'); ?>" target="_self">Advertise With Us</a></li>
-            <li><a title="Report" href="<?php echo base_url('report-abuse'); ?>" target="_self">Report </a></li>
             <li><a href="<?php echo base_url('feedback'); ?>" target="_self">Feedback</a></li>
-            <li><a href="<?php echo base_url('terms-and-condition'); ?>" target="_self">Terms &amp; Condition </a></li>
+			<li><a href="<?php echo base_url('terms-and-condition'); ?>" target="_self">Terms &amp; Condition </a></li>
+			
             <li><a href="<?php echo base_url('privacy-policy'); ?>" target="_self">Privacy Policy</a></li>
+			<li><a title="Report" href="<?php echo base_url('report-abuse'); ?>" target="_self">Report </a></li>
             <li><a title="Disclaimer Policy" href="<?php echo base_url('disclaimer-policy'); ?>" target="_self">Disclaimer Policy</a></li>
         </ul>
 
