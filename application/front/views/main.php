9c8b531a007acbd7605dc32075635837773cb446
harshad-aileensoul <harshad.aileensoul@gmail.com>
2019-06-21 19:57:00 +0530

    css and main.php

--- application/front/views/main.php
+++ application/front/views/main.php
@@ -114,7 +114,7 @@ header("Pragma: no-cache"); // HTTP/1.0
 					<div class="col-md-6 col-sm-6">
 						<div class="right-reg">
 							<div class="right-reg-content">
-								<p>A place where everyone can learn, earn and grow</p>
+								<p>A place where everyone can learn, earn and grow.</p>
 								
 								<div class="reg-btns">
 									<a href="javascript:void(0);" id="login-btn-id" class="btn-new-1">Login</a>
